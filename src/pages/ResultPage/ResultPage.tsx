fcdfee7b15ec147e389b435c054caedf1e3246c6
shobit1337 <37979367+shobit1337@users.noreply.github.com>
2022-05-22 17:09:03 +0530

    feat: result page functionality

--- src/pages/ResultPage/ResultPage.tsx
+++ src/pages/ResultPage/ResultPage.tsx
@@ -1,18 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { ResultCard } from "../../components";
+import { useAuth } from "../../context/AuthContext";
+import { getQuiz } from "../../services/quizServices";
+import { getResult } from "../../services/resultServices";
+import { QuizResultType, QuizType } from "../../types/quizTypes";
 
 function ResultPage() {
+  const { currentUser } = useAuth();
+  const { resultId } = useParams();
+  const navigate = useNavigate();
+
+  const [resultData, setResultData] = useState({} as QuizResultType);
+  const [quizData, setQuizData] = useState({} as QuizType);
+
+  useEffect(() => {
+    if (resultId) {
+      (async () => {
+        const resultRes = await getResult(resultId);
+        if (resultRes.uid) {
+          setResultData(resultRes);
+          const quizRes = await getQuiz(resultRes.quizId);
+          if (quizRes.uid) setQuizData(quizRes);
+        } else {
+          navigate("-1");
+        }
+      })();
+    } else {
+      navigate("-1");
+    }
+  }, []);
+
   return (
     <>
-      <h2 className="text-center m-sm">JavaScript Quiz Results</h2>
+      <h2 className="text-center m-sm">{quizData.title} Result</h2>
       {/* <!-- Result Container --> */}
       <div className="d-flex flex-column flex-center gap-sm m-auto">
         {/* <!-- Result Header --> */}
         <div className="d-flex score-header m-md">
-          <span className="text-semibold">Time Taken: 15 mins</span>
           <span className="text-semibold">
-            Total Score: <span className="text-success"> 50 pts</span>
+            Total Time: {quizData.timelimit} seconds
+          </span>
+          <span className="text-semibold">
+            Total Score:{" "}
+            <span className="text-success"> {resultData.score} pts</span>
           </span>
         </div>
       </div>
@@ -20,13 +51,16 @@ function ResultPage() {
       {/* <!-- Actual Results --> */}
       <div className="d-flex flex-column result-container">
         {/* ResultCard */}
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
-        <ResultCard />
+        {quizData.questions?.map((question, index) => (
+          <ResultCard
+            key={index}
+            quizQuestion={question}
+            result={Number(resultData.options[index])}
+            index={index}
+          />
+        ))}
       </div>
+
       <div className="d-flex justify-between px-sm">
         <Link className="btn btn-rounded btn-sm m-sm" to="-1">
           Go Back
